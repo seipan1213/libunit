f2372a3f4385bf83fe17db1216039946fe0a1f6b
Tomoya Ishigaki <tishigak@c1r11s2.42tokyo.jp>
2021-05-12 16:17:41 +0900

    #2 add atoi test

--- tests/atoi/00_launcher.c
+++ tests/atoi/00_launcher.c
@@ -6,7 +6,7 @@
 /*   By: tishigak <tishigak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 22:54:19 by tishigak          #+#    #+#             */
-/*   Updated: 2021/05/12 15:26:06 by tishigak         ###   ########.fr       */
+/*   Updated: 2021/05/12 15:57:24 by tishigak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,15 @@ int	atoi_launcher(void)
 	load_test(&tests, "Minus test", &atoi_minus_test);
 	load_test(&tests, "Max test", &atoi_max_test);
 	load_test(&tests, "Min test", &atoi_min_test);
+	load_test(&tests, "Long max test", &atoi_longmax_test);
+	load_test(&tests, "Long min test", &atoi_longmin_test);
+	load_test(&tests, "Long over test", &atoi_longover_test);
+	load_test(&tests, "Long under test", &atoi_longunder_test);
+	load_test(&tests, "Sign test", &atoi_sign_test);
+	load_test(&tests, "Space test", &atoi_space_test);
+	load_test(&tests, "Comb test", &atoi_comb_test);
+	load_test(&tests, "Notnum test", &atoi_notnum_test);
+	load_test(&tests, "Notnum2 test", &atoi_notnum2_test);
+	load_test(&tests, "Empty test", &atoi_empty_test);
 	return (launch_tests(&tests));
 }
--- tests/atoi/atoi.h
+++ tests/atoi/atoi.h
@@ -6,7 +6,7 @@
 /*   By: tishigak <tishigak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 22:52:14 by tishigak          #+#    #+#             */
-/*   Updated: 2021/05/11 18:58:45 by tishigak         ###   ########.fr       */
+/*   Updated: 2021/05/12 15:54:20 by tishigak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,15 @@ int	atoi_zero_test(void);
 int	atoi_minus_test(void);
 int	atoi_max_test(void);
 int	atoi_min_test(void);
+int	atoi_longmax_test(void);
+int	atoi_longmin_test(void);
+int	atoi_longover_test(void);
+int	atoi_longunder_test(void);
+int	atoi_sign_test(void);
+int	atoi_space_test(void);
+int	atoi_comb_test(void);
+int	atoi_notnum_test(void);
+int	atoi_notnum2_test(void);
+int	atoi_empty_test(void);
 
 #endif
